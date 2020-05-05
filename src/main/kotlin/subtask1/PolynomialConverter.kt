2b86fc1da18ac422fab88c049444fda902819903
anna-d-k <annakhomchenko@coherentsolutions.com>
2020-05-05 23:10:17 +0300

    Subtask1

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -1,9 +1,30 @@
 package subtask1
 
+import kotlin.math.abs
+
 class PolynomialConverter {
 
-    // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        if (numbers.isNotEmpty())
+            return numbers.foldIndexed("") { index, result, item ->
+                val st =
+                    when (index) {
+                        numbers.lastIndex -> ""
+                        numbers.lastIndex - 1 -> "x"
+                        else -> "x^${numbers.lastIndex - index}"
+                    }
+                val sign =
+                    when {
+                        item < 0 -> " - "
+                        (item > 0) and (index != 0) -> " + "
+                        else -> ""
+                    }
+                when (item) {
+                    0 -> result
+                    1, -1 -> "${result}${sign}${st}"
+                    else -> "$result${sign}${abs(item)}${st}"
+                }
+            }.trim()
+        else return null
     }
 }
