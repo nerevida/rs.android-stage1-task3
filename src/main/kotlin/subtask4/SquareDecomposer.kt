f2e9b0e506b78f45e491ef828f2dd7ffa18f5a69
anna-d-k <annakhomchenko@coherentsolutions.com>
2020-05-07 00:42:01 +0300

    Subtask4 not tail

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -2,8 +2,29 @@ package subtask4
 
 class SquareDecomposer {
 
-    // TODO: Complete the following function
+
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        var s = getSqrts(number * number, number)
+        return s
+
     }
+
+    fun getSqrts(number: Int, q: Int): Array<Int>? {
+//       if (number == 1) {
+//           return arrayOf(1)
+//       }
+        loop@ for (i in q - 1 downTo 2) {
+            val next = number - i * i
+            val result =
+                when {
+                    next == 0 -> arrayOf(i)
+                    next == 1 -> arrayOf(1).plus(i)
+                    next > 0 -> getSqrts(next, i)?.plus(i)
+                    else -> null
+                }
+            if (result != null) return result
+        }
+        return null
+    }
+
 }
