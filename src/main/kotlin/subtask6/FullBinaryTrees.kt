83a5c82712afd692a4300b4b0fc3b2c641bd50e0
anna-d-k <annakhomchenko@coherentsolutions.com>
2020-05-07 21:58:18 +0300

    Subtask6 draft

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -2,8 +2,33 @@ package subtask6
 
 class FullBinaryTrees {
 
-    // TODO: Complete the following function
+
     fun stringForNodeCount(count: Int): String {
-        throw NotImplementedError("Not implemented")
+        return if ((count > 0) and (!count.isEven())) {
+            var trees = ArrayList<BiTree>()
+            for (i in 1..count step 2) {
+                var oneMoreTree = BiTree(null)
+
+                // todo implement
+
+                trees.add(oneMoreTree)
+            }
+            return trees.toString()
+        } else
+            "[]"
+    }
+
+    class BiTree(private var child: BiTree?) {
+        private val value: String = "0"
+
+        override fun toString(): String {
+            return if (child == null) "[${value}]" else "[${value},${child.toString()},${child.toString()}]"
+        }
     }
 }
+
+private fun Int.isEven(): Boolean {
+    return this % 2 == 0
+}
+
+
